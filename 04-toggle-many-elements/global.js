907b170d727c7411c78d90cfae9943cbdd3c6f10
Krasnikovs <ralis3333@gmail.com>
2021-03-23 16:27:00 +0200

    04

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,16 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    const toggleButton = document.getElementById('button')
+    toggleButton.addEventListener('click', function(){
+
+    var toggleText = document.getElementsByClassName('toggle_me')
+    Array.from(toggleText).forEach(function(toggleText){
+    if(toggleText.style.display === 'none') {
+        toggleText.style.display = 'block';
+    } else {
+        toggleText.style.display = 'none'
+    }
+    })
+  })
+});
